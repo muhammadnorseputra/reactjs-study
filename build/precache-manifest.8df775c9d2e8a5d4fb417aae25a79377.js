d0bd535a46bfd390c1e38dad7d1a6ca54c4134d4
muhammadnorseputra <muhammadnorseputra@gmail.com>
2020-08-30 22:03:03 +0700

    fixs to deploy

---
+++ build/precache-manifest.8df775c9d2e8a5d4fb417aae25a79377.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e24e3093283480ce3dfbf241bb922575",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2d90d9ed9a165ab5c23d",
+    "url": "/static/css/2.bee48fb1.chunk.css"
+  },
+  {
+    "revision": "ce696914eb52860d0fa7",
+    "url": "/static/css/3.9d49a0b9.chunk.css"
+  },
+  {
+    "revision": "a174e58fee5502ca88be",
+    "url": "/static/css/4.acbc7791.chunk.css"
+  },
+  {
+    "revision": "2d90d9ed9a165ab5c23d",
+    "url": "/static/js/2.02995d3b.chunk.js"
+  },
+  {
+    "revision": "efcc7ff0fbcc448bc0d9342e760917b3",
+    "url": "/static/js/2.02995d3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce696914eb52860d0fa7",
+    "url": "/static/js/3.5b9ab964.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/3.5b9ab964.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a174e58fee5502ca88be",
+    "url": "/static/js/4.af389b91.chunk.js"
+  },
+  {
+    "revision": "51e213324514ca839e0b",
+    "url": "/static/js/main.17ecae62.chunk.js"
+  },
+  {
+    "revision": "7c430242bcd99d73e13c",
+    "url": "/static/js/runtime-main.0d6724b6.js"
+  },
+  {
+    "revision": "80e33249761cfbbc667dacb5c30a70da",
+    "url": "/static/media/logo-big.80e33249.png"
+  },
+  {
+    "revision": "58448bc8344e86403c7ad25b3509965f",
+    "url": "/static/media/ntailu.58448bc8.ttf"
+  },
+  {
+    "revision": "3350b74ee179d80520af3ebee4349ae7",
+    "url": "/static/media/ntailub.3350b74e.ttf"
+  },
+  {
+    "revision": "264cde8bda2768526a99799504c573cc",
+    "url": "/static/media/undraw_secure_login_pdn4.264cde8b.svg"
+  }
+]);
